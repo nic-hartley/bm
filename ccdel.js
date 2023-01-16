74f45ac958def0580c4efc1d095238a1e8ad66a4
Nic Hartley <the@redfennec.dev>
2023-01-16 12:24:39 -0500

    nicer logging

--- ccdel.js
+++ ccdel.js
@@ -2,7 +2,7 @@
 // fetch("https://nic-hartley.github.io/bm/ccdel.js").then(r=>r.text()).then(b=>eval(b))
 (_=>{
   let deleted = 0;
-  console.log("Running CCdel...");
+  console.log("CCdel: initializing");
   setInterval(_ => {
     let deleted = 0;
     document.querySelectorAll(".user-notice-line:not(.__ccdel-safe)").forEach(e => {
@@ -16,7 +16,7 @@
   }, 100);
   setInterval(_ => {
     if (deleted > 0) {
-      console.log(`Deleted ${deleted} in the past 5 seconds`);
+      console.log(`CCdel: ${deleted} in the past 5 seconds`);
       deleted = 0;
     }
   }, 5000);
